2fbdfbd2c79316fa6f2cc6d1dbf04397b9378267
Gkxd <dhua@andrew.cmu.edu>
2015-02-24 20:56:36 -0500

    Added basic framework for syncing objects with beat

---
+++ Assets/Resources/Scripts/_AbstractRhythmObject.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public abstract class _AbstractRhythmObject : MonoBehaviour {
+
+    private AudioSource audioSource;
+    private AudioClip audioClip;
+
+    private int BPM;
+    private float samplesPerBeat;
+    private int lastBeatUpdate = -1;
+
+
+    public void Start() {
+        GameObject bgmContainer = GameObject.FindGameObjectWithTag("BackgroundMusic");
+
+        audioSource = bgmContainer.audio;
+        audioClip = audioSource.clip;
+
+        BPM = bgmContainer.GetComponent<MusicWrapper>().BPM;
+
+        samplesPerBeat = 60 * audioClip.frequency/BPM;
+    }
+
+    public void Update() {
+        int beat = (int)(audioSource.timeSamples / samplesPerBeat);
+
+        if (beat != lastBeatUpdate) {
+            lastBeatUpdate = beat;
+            rhythmUpdate(beat);
+        }
+    }
+
+    public abstract void rhythmUpdate(int beat);
+}

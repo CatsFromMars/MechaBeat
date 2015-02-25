5e51c8e80497720648f92153b63fda052e2326bb
Gkxd <dhua@andrew.cmu.edu>
2015-02-24 20:59:38 -0500

    Cleaned up music wrapper

--- Assets/Resources/Scripts/MusicWrapper.cs
+++ Assets/Resources/Scripts/MusicWrapper.cs
@@ -4,27 +4,4 @@ using System.Collections;
 public class MusicWrapper : MonoBehaviour {
     /* Public serializable variables */
     public int BPM;
-    public int beatCount;
-
-    /* Private variables */
-
-    private AudioSource audioSource;
-    private AudioClip audioClip;
-
-    private float beatLength;
-
-    private int beatNumber;
-
-    public void Start() {
-        audioSource = gameObject.audio;
-        audioClip = audioSource.clip;
-
-        beatLength = 60.0f / BPM;
-    }
-
-    public void Update() {
-        //Debug.Log(audioSource.timeSamples/(int)(beatLength * audioClip.frequency));
-    }
-
-
 }

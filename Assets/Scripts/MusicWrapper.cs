612cee2a0551d49277c21e70b8fd56412a6ad228
Gkxd <dhua@andrew.cmu.edu>
2015-02-28 03:51:07 -0500

    Added support for non-looping music

--- Assets/Scripts/MusicWrapper.cs
+++ Assets/Scripts/MusicWrapper.cs
@@ -4,4 +4,27 @@ using System.Collections;
 public class MusicWrapper : MonoBehaviour {
     /* Public serializable variables */
     public int BPM;
+    public float introTime;
+    public float loopThreshold;
+
+    private AudioSource audioSource;
+    private AudioClip audioClip;
+    private float audioLength;
+    private float loopTime;
+
+    public void Start() {
+        audioSource = gameObject.audio;
+        audioClip = audioSource.clip;
+        audioLength = audioClip.length;
+
+        loopTime = audioLength - introTime;
+    }
+
+    public void Update() {
+        if (introTime > 0 && loopThreshold > 0) {
+            if (audioSource.timeSamples > loopThreshold * audioClip.frequency) {
+                audioSource.timeSamples -= Mathf.RoundToInt(loopTime * audioClip.frequency);
+            }
+        }
+    }
 }

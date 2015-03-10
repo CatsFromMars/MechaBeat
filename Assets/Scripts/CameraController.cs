a8d7d81fdecb50bf3ec890315f9e4ea35d4f9696
Torqued <bf.torqued@gmail.com>
2015-03-10 17:03:51 -0400

    Fixed Camera in Level 2(separate Object instead of player Child)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = new Vector3 (0, 0, -10);
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}

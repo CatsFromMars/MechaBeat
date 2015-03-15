18df6c6c67876547ae232c049105916a9e780e9a
Jordan Tick <jrtick@andrew.cmu.edu>
2015-03-15 14:25:32 -0400

    possible new mechanic?

---
+++ Assets/Scripts/clockRotation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clockRotation : MonoBehaviour {
+
+	public float speed = 180f;
+	private Vector3 axle = new Vector3(0,1,0);
+	
+	
+	void Update ()
+	{
+		transform.Rotate(axle, speed * Time.deltaTime);
+	}
+}
--- Assets/Scripts/gearRotation.cs
+++ Assets/Scripts/gearRotation.cs
@@ -4,10 +4,11 @@ using System.Collections;
 public class gearRotation : MonoBehaviour {
 
 	public float speed = 180f;
+	private Vector3 axle = new Vector3(0,0,1);
 	
 	
 	void Update ()
 	{
-		transform.Rotate(new Vector3(0,0,1), speed * Time.deltaTime);
+		transform.Rotate(axle, speed * Time.deltaTime);
 	}
 }

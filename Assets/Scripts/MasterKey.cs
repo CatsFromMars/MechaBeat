ed786d186c1d4bb6bf4ba8e41c2227e2811c735b
Torqued <bf.torqued@gmail.com>
2015-03-17 17:34:42 -0400

    Changed Levels and added Keys

---
+++ Assets/Scripts/MasterKey.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MasterKey : MonoBehaviour {
+
+	public int numKeys;
+	public int collectedKeys;
+
+	// Use this for initialization
+	void Start () {
+		numKeys = 4;
+		collectedKeys = 0;
+	}
+	
+	// Update is called once per frame
+	public void incrementCollectedKeys () {
+		collectedKeys++;
+		if (collectedKeys == numKeys) {
+			//opendoor
+		}
+	}
+	
+}

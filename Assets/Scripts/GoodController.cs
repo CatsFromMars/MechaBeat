9a536ebb8142476af24d5f95c595bd40135cbe5a
Madeline Duque <mduque@andrew.cmu.edu>
2015-03-19 20:59:13 -0400

    Official Level 1 Done

--- Assets/Scripts/GoodController.cs
+++ Assets/Scripts/GoodController.cs
@@ -4,6 +4,10 @@ using Rhythmify;
 
 public class GoodController : _AbstractRhythmObject {
 
+	private GameObject controller;
+	private HashIDs hash;
+	private Animator animator;
+
     public int numJumps;
     public float jumpHeight;
 
@@ -19,12 +23,19 @@ public class GoodController : _AbstractRhythmObject {
     private bool inAir;
     private bool touchedFloor;
 
+	void Awake() {
+		controller = GameObject.FindGameObjectWithTag("GameController");
+		hash = controller.GetComponent<HashIDs>();
+		animator = GetComponent<Animator>();
+	}
+
     void FixedUpdate() {
         Vector3 nextVelocity = new Vector3(movement, rigidbody.velocity.y, 0);
 
         if (jumping) {
             nextVelocity.y = jumpHeight;
             jumping = false;
+			animator.SetBool(hash.jumpBool, false);
         }
 
         rigidbody.velocity = nextVelocity;
@@ -39,8 +50,19 @@ public class GoodController : _AbstractRhythmObject {
     override protected void asyncUpdate() {
         movement = Input.GetAxis("Horizontal") * moveSpeed;
 
+		if (movement > 0) {
+			transform.rotation = Quaternion.Euler(0, 90, 0);
+			animator.SetBool(hash.runningBool, true);
+		} else if (movement < 0) {
+			transform.rotation = Quaternion.Euler(0, -90, 0);
+			animator.SetBool(hash.runningBool, true);
+		} else if (movement == 0) {
+			animator.SetBool(hash.runningBool, false);
+		}
+
         if (Input.GetKeyDown(KeyCode.Space) && jumpsLeft > 0) {
             jumping = true;
+			animator.SetBool(hash.jumpBool, true);
             jumpsLeft--;
         }
     }
@@ -54,6 +76,9 @@ public class GoodController : _AbstractRhythmObject {
     }
 
     override protected void rhythmUpdate(int beat) {
-        
-    }
+		//Sync player animations to the music
+		if (animator.GetCurrentAnimatorStateInfo(0).nameHash == hash.idleState) {
+			animator.SetTrigger(hash.beatTrigger);
+		}
+	}
 }

9f39989cad4efe06e588d60b45e3470cfe6edab6
Jordan Tick <jrtick@andrew.cmu.edu>
2015-03-20 00:44:47 -0400

    Adds Level 5 (boss level)

---
+++ Assets/move.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+	public GameObject key1;
+	public GameObject key2;
+	
+	// Update is called once per frame
+	void Update () {
+		if((key1==null) & (key2==null))
+			transform.rotation=Quaternion.Euler(new Vector3(365,267,94));
+	}
+}

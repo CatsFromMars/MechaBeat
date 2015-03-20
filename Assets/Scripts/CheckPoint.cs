283b11e245e051fe90eeec7dec91abb92fad97de
Gkxd <dhua@andrew.cmu.edu>
2015-03-20 00:54:04 -0400

    implemented checkpoints

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPoint : MonoBehaviour {
+    void OnTriggerEnter(Collider collision) {
+        if (collision.gameObject.tag == "Player") {
+            collision.gameObject.GetComponent<GoodController>().setCheckPoint(transform.position);
+        }
+    }
+}

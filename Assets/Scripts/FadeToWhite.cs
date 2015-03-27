ef1614d175afa8b695013e5dca8ba077f8bbbe0e
Torqued <bf.torqued@gmail.com>
2015-03-27 04:13:52 -0400

    Fader

---
+++ Assets/Scripts/FadeToWhite.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeToWhite : MonoBehaviour {
+
+	private GameObject gameController;
+	// Use this for initialization
+	void Awake () {
+		gameController = GameObject.FindGameObjectWithTag ("GameController");
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") {
+			MasterKey m = gameController.GetComponent<MasterKey>();
+			if(m.canOpen())
+			{
+				//Application.LoadLevel(nextLevel);
+				GameObject.FindGameObjectWithTag("WhiteFade").GetComponent<Animator>().SetTrigger("FadeIn");
+			}
+			
+		}
+	}
+
+	public void fader()
+	{
+
+	}
+}
